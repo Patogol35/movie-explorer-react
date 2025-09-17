0b3e2f93b66d797f2c2bfb68a530d1306779c3f1
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-17 11:13:37 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import MovieDetail from './pages/MovieDetail'
-export default function App() {
+import NotFound from './pages/NotFound'
+
+export default function App({ toggleMode, mode }) {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home toggleMode={toggleMode} mode={mode} />} />
       <Route path="/movie/:id" element={<MovieDetail />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
